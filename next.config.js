6303544a9e9995487e8a2d47b10dbe394b139e94
Yago Garcez <yago.garcez@outlook.com>
2023-12-08 21:14:37 -0300

    Implementando seção de avaliações.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['firebasestorage.googleapis.com', 'm-cdn.phonearena.com', 'www.fastshop.com.br', 'i.ebayimg.com', 'trocafone.vtexassets.com', 'encrypted-tbn0.gstatic.com', 'encrypted-tbn2.gstatic.com']
+        domains: ['firebasestorage.googleapis.com', 'm-cdn.phonearena.com', 'www.fastshop.com.br', 'i.ebayimg.com', 'trocafone.vtexassets.com', 'encrypted-tbn0.gstatic.com', 'encrypted-tbn2.gstatic.com', 'lh3.googleusercontent.com']
     }
 }
 
