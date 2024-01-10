e5680fd69b9621e4c1a0e03e060b4058895fabe0
Yago Garcez <yago.garcez@outlook.com>
2024-01-09 23:58:02 -0300

    Creating CartClient Component

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,5 +1,6 @@
 import { CartProductType } from "@/app/product/[productId]/ProductDetails";
-import { createContext, useCallback, useContext, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import { toast } from 'react-hot-toast';
 
 type CartContextType = {
     cartTotalQty: number;
@@ -17,6 +18,13 @@ export const CartContextProvider = (props: Props) => {
     const [cartTotalQty, setCartTotalQty] = useState(0);
     const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(null);
 
+    useEffect(() => {
+        const cartItems: any = localStorage.getItem('eShopCartItems');
+        const cProducts: CartProductType[] | null = JSON.parse(cartItems);
+
+        setCartProducts(cProducts);
+    }, []);
+
     const handleAddProductToCart = useCallback((product: CartProductType) => {
         setCartProducts((prev) => {
             let updatedCart;
@@ -28,6 +36,8 @@ export const CartContextProvider = (props: Props) => {
                 updatedCart = [product]
             }
 
+            toast.success('Product added to cart.');
+            localStorage.setItem('eShopCartItems', JSON.stringify(updatedCart));
             return updatedCart;
         });
     }, []);
